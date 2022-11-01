6c8dbf6d296acc96e4352e6bd9fc95da59a90185
Sabir Hassan <sabirbk06@gmail.com>
2022-11-02 00:32:52 +0500

    remove old components and pages and add new empty layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/navbar"
-import Footer from "./components/footer"
-import Home from "./pages/home"
-import Experience from "./pages/experience"
-import Projects from "./pages/projects"
-import ProjectDisplay from "./pages/projectDisplay"
+import Layout from "./components/Layout"
 import "./App.scss"
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/" element={<Layout />}></Route>
         </Routes>
-        <Footer />
       </Router>
     </div>
   )
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,7 @@
+import "./index.scss"
+
+const Layout = () => {
+  return <>hello</>
+}
+
+export default Layout
