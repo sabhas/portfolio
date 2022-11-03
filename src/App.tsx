8b8faeaff839d1f6ae2546b2deafdcaa7e572b39
Sabir Hassan <sabirbk06@gmail.com>
2022-11-04 02:01:27 +0500

    add about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
+import About from "./components/About"
 import "./App.scss"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
           </Route>
         </Routes>
       </Router>
