eba1d0ae1e640bd6b021b8035da8496c87f92048
Sabir Hassan <sabirbk06@gmail.com>
2022-11-16 01:32:51 +0500

    use hash router instead of browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
 import About from "./components/About"
