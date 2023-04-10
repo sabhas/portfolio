c9e012352c708c24885f42a15c41aa3c1c579bdd
Sabir Hassan <sabirbk06@gmail.com>
2023-04-11 01:30:57 +0500

    add blog to projects list

--- src/components/Portfolio/projectList.ts
+++ src/components/Portfolio/projectList.ts
@@ -1,19 +1,27 @@
-import wordle from "../../assets/images/wordle.jpg"
-import todo from "../../assets/images/todo.png"
+import blog from '../../assets/images/blog.png'
+import wordle from '../../assets/images/wordle.jpg'
+import todo from '../../assets/images/todo.png'
 
 export const ProjectList = [
   {
-    name: "Wordle",
-    image: wordle,
-    appLink: "https://wordle.sabhas.me",
-    githubLink: "https://github.com/sabhas/Wordle",
-    skills: "React, Typescript, HTML, CSS",
+    name: 'Blog',
+    image: blog,
+    appLink: 'https://blog.sabhas.me',
+    githubLink: 'https://github.com/sabhas/gatsby-blog',
+    skills: 'React, Typescript, HTML, CSS'
   },
   {
-    name: "Todo",
-    image: todo,
-    appLink: "https://todo.sabhas.me",
-    githubLink: "https://github.com/sabhas/todo-list",
-    skills: "React, Typescript, HTML, CSS",
+    name: 'Wordle',
+    image: wordle,
+    appLink: 'https://wordle.sabhas.me',
+    githubLink: 'https://github.com/sabhas/Wordle',
+    skills: 'React, Typescript, HTML, CSS'
   },
+  {
+    name: 'Todo',
+    image: todo,
+    appLink: 'https://todo.sabhas.me',
+    githubLink: 'https://github.com/sabhas/todo-list',
+    skills: 'React, Typescript, HTML, CSS'
+  }
 ]
